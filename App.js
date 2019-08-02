551735fb3f2f61a03c4397c89d5919d2eb6ac80d
ConteAlexandre <theluffy@hotmail.fr>
2019-08-02 10:33:56 +0200

    ajout des composants redux et mis en place du store

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import SearchScreen from "./screen/search-screen";
+import store from "./store";
+import {Provider} from "react-redux";
 
 class App extends Component {
 
     render() {
         return (
-                <SearchScreen/>
+            <Provider store={store} ><SearchScreen/></Provider>
         );
     }
 }
