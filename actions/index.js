54687cbe1776ca7e6d85af5763784ffb4f1b7774
ConteAlexandre <theluffy@hotmail.fr>
2019-08-03 11:16:40 +0200

    ajout de la première étape pour écrire dans le store pour l'api

---
+++ actions/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import {API_KEY} from "../constant";
+import {SET_CURRENT_WEATHER} from "./actionstypes";
+const WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+
+export const getUserByCity = city =>
+    async dispatch => {
+        const response = await axios.get(`${WEATHER_BASE_URL}?q=${city}?appid=${API_KEY}`);
+        dispatch({ type: SET_CURRENT_WEATHER, payload: response.data })
+    }
--- reducers/wheather.js
+++ reducers/wheather.js
@@ -1,5 +1,11 @@
+import {SET_CURRENT_WEATHER} from "../actions/actionstypes";
+
 export function WeatherReducer(state, action) {
-    return {
-        data: "blabla"
+    if (action.type === SET_CURRENT_WEATHER) {
+        return {
+            data: action.payload
+        }
     }
+
+    return state
 }
