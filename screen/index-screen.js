741f93b66137806ce3e1db1f4d35abe4ac9ba8b6
ConteAlexandre <theluffy@hotmail.fr>
2019-09-16 13:05:54 +0200

    ajout de commentaires et fin sur le fichier index-screen dans le dossier screens

--- screen/index-screen.js
+++ screen/index-screen.js
@@ -1,14 +1,27 @@
-import React, {Component} from 'react';
-import {View} from "react-native";
-import { connect } from "react-redux";
-import { withNavigation } from "react-navigation";
-import { facebookLogin } from "../actions";
-import {AsyncStorage} from "react-native";
+//Voici notre fichier qui établi quel écran on voit en premier lorsqu'on lance l'api pour la première fois
 
+//Importation du paquet React pour pouvoir utiliser les composants react-native
+import React, {Component} from 'react';
+
+//Importation des composants react-native
+import {View, AsyncStorage} from "react-native";
+
+//Importation du paquet connect pour pouvoir utilise la librairie redux
+import { connect } from "react-redux";
+
+//withNavigation pour pouvoir naviguer sur les différents écrans de l'api
+import { withNavigation } from "react-navigation";
+
+//Importation de notre composant facebooklogin
+import { facebookLogin } from "../actions";
+
+
+//Commençons notre composant
 class IndexScreen extends Component {
 
+    //Nous commençons par définit les états à recevoir avec leurs mises a jour et le comportement
     componentDidMount() {
-        //connexion fb
+        //connexion fb,
         AsyncStorage.getItem("fbtoken").then( token => {
             if (token) {
                 //Si token deja existant vers Search
@@ -20,9 +33,13 @@ class IndexScreen extends Component {
         })
     }
 
+    //Définition de la fonction qui nous envoi vers l'écran de recherche de ville
     goToSearch = () => {
         this.props.navigation.push("Search")
     }
+
+    //Meme si le rendu est vierge il le faut pour la bonne procédure de l'api car sans ça elle ne sera
+    // pas comment rendre le rendu
     render() {
         return (
             <View/>
@@ -30,12 +47,15 @@ class IndexScreen extends Component {
     }
 }
 
+//
 const mapToStateToProps = state => {
     return {};
 };
 
+//Ici quel action va intéragir avec nos états et propriétés de cet écran
 const mapDispatchToProps = {
     facebookLogin
 };
 
+//On exporte le tout, avec withNavigation pour permettre le switch d'ecran et connect pour utilise la librairies redux
 export default withNavigation(connect(mapToStateToProps, mapDispatchToProps)(IndexScreen));
